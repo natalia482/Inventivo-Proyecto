0203c053ee9ac41f4763abbc13f0f38bd054517d
natalia482 <nataliavillalobos482@gmail.com>
2025-05-23 23:02:52 -0500

    apis

--- MODELO/CRUD/UsuariologinAPI.php
+++ MODELO/CRUD/UsuariologinAPI.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 header("Content-Type: application/json");
-header("Access-Control-Allow-Origin: http://localhost"); // ajusta tu dominio frontend
+header("Access-Control-Allow-Origin: https://tudominio.netlify.app");
 header("Access-Control-Allow-Credentials: true");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
